a708aadf865c7b4a46bb1a991c8d9be5a9ddcbb5
orian-georgian <orian.georgian@yahoo.com>
2016-06-18 23:34:29 +0300

    Delete Users

--- License/Mapping/MembersMap.cs
+++ License/Mapping/MembersMap.cs
@@ -12,7 +12,10 @@ namespace License.Mapping
             Map(x => x.Role);
             Map(x => x.MemberFunction);
             Map(x => x.PersonalPage);
-            HasOne(x => x.User).Cascade.All().PropertyRef("Member");
+            Map(x => x.Email);
+            Map(x => x.Password);
+            Map(x => x.AuthRole);
+            Map(x => x.AuthToken);
             HasMany<Lectures>(x => x.Lectures).Inverse().AsBag();
             HasManyToMany<Projects>(x => x.Projects)
                 .Cascade.All()
--- License/Model/Members.cs
+++ License/Model/Members.cs
@@ -9,17 +9,24 @@ namespace License.Model
         public virtual string Role { get; set; }
         public virtual string MemberFunction { get; set; }
         public virtual string PersonalPage { get; set; }
-        public virtual Users User { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string AuthRole { get; set; }
+        public virtual string AuthToken { get; set; }
         public virtual IList<Lectures> Lectures { get; set; }
         public virtual IList<Projects> Projects { get; set; }
         public virtual IList<Publications> Publications { get; set; }
 
-        public Members(string fullName, string role, string function, string page)
+        public Members(string fullName, string role, string function, string page, string email, string password, string authRole, string authToken)
         {
             this.FullName = fullName;
             this.Role = role;
             this.MemberFunction = function;
             this.PersonalPage = page;
+            this.Email = email;
+            this.Password = password;
+            this.AuthRole = AuthRole;
+            this.AuthToken = authToken;
             this.Lectures = new List<Lectures>();
             this.Projects = new List<Projects>();
             this.Publications = new List<Publications>();
