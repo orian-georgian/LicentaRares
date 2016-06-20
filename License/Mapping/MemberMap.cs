255ec162682fea792d579721dbf4dc62fed25831
orian-georgian <orian.georgian@yahoo.com>
2016-06-21 02:52:27 +0300

    Fix lecture map

--- License/Mapping/MemberMap.cs
+++ License/Mapping/MemberMap.cs
@@ -15,7 +15,7 @@ namespace License.Mapping
             Map(x => x.PersonalPage);
             Map(x => x.Email);
             Map(x => x.MemberPhoto);
-            HasMany<Lecture>(x => x.Lectures).Cascade.All();
+            HasMany<Lecture>(x => x.Lectures).Inverse().Cascade.SaveUpdate();
             HasManyToMany<Project>(x => x.Projects)
                 .Cascade.All()
                 .Table("MembersProjects");
